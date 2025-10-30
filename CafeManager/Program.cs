0c2eaa01e25c0aec184a4bcda7a1f784bfaad883
JW33R <JW33R@TYLERPC>
2025-10-30 00:40:32 -0400

    Add project files.

---
+++ CafeManager/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CafeManager
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Menu MenuSelect = new Menu();
+            MenuSelect.MenuSelection();
+        }
+        
+            
+        
+    
+    }
+        
+        
+}
