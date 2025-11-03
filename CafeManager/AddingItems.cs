c35657784dbec0ffefcd6504f985a1d9f54234ce
JW33R <JW33R@DESKTOP-6I0O4P4>
2025-11-03 14:05:26 -0500

    Revert "commit all repos"
    This reverts commit a38a6dd8415689680ce9aac745f2d9a06edc0cd8.

---
+++ CafeManager/AddingItems.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace CafeManager
+{
+    internal class AddingItems
+    {
+        public static void AddItem()
+        {
+            List<string> Items = new List<string>();
+            List<string> Price = new List<string>();
+            List<string> Quanity = new List<string>();
+            string Decision1 = "";
+            string Decision2 = "";
+            string Decision3 = "";
+            while (Decision1.ToUpper() != "DONE")
+            {
+                Console.WriteLine("Which item would you like to Add? Type \"Done\" when finsihed.");
+                Decision1 = Console.ReadLine();
+                if (Decision1.ToUpper() == "DONE")
+                {
+                    goto EndLoop;
+                }
+                if (Decision1.TrimStart() == "")
+                {
+                    Console.WriteLine("You can't do that.");
+                    AddItem();
+                }
+                Console.WriteLine("What is the Price?");
+                double ConvertDecision2 = Convert.ToDouble(Decision1 = Console.ReadLine());
+                if (Decision2.TrimStart() == " ")
+                {
+                    Console.WriteLine("You can't do that.");
+                    AddItem();
+                }
+                Console.WriteLine("How Many?");
+                int ConvertDecision3 = Convert.ToInt32(Decision3 = Console.ReadLine());
+                if (Decision3.TrimStart() == "")
+                {
+                    Console.WriteLine("You can't do that.");
+                    AddItem();
+                }
+                Items.Add(Decision1);
+                Price.Add(Decision2);
+                Quanity.Add(Decision3);
+                foreach (var num in Items)
+                {
+                    Console.WriteLine($"You added {Items} || {Price} || {Quanity}");
+                }
+            }
+        EndLoop:
+            Console.WriteLine("");
+            
+            
+
+
+
+
+
+
+        }        
+    }
+}
---
+++ CafeManager/RemovingItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeManager
+{
+    internal class RemovingItems
+    {
+        public static void RemoveItems()
+        {
+
+        }
+    }
+}
